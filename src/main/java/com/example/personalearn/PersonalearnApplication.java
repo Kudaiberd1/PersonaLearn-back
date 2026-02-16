03056874f5bf3bcb39f3e9b5066cea3e71150489
Kudaiberdi <kdajberdiz@gmail.com>
2026-02-16 20:45:52 +0500

    keycloak authentication

---
+++ src/main/java/com/example/personalearn/PersonalearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.personalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalearnApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/personalearn/controller/AuthController.java
@@ -0,0 +1,51 @@
+package com.example.personalearn.controller;
+
+import com.example.personalearn.dto.request.UserAuthRequest;
+import com.example.personalearn.dto.response.AuthResponse;
+import com.example.personalearn.dto.response.UserDetailsResponse;
+import com.example.personalearn.service.KeycloakService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.Valid;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private final KeycloakService keycloakService;
+
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDetailsResponse> me(@AuthenticationPrincipal Jwt jwt) {
+        UserDetailsResponse userDetails = keycloakService.getUserDetails(jwt);
+        return ResponseEntity.ok(userDetails);
+    }
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AuthResponse> login(@RequestBody @Valid UserAuthRequest userAuthRequest) throws BadRequestException {
+        AuthResponse authResponse = keycloakService.getAuthResponse(userAuthRequest.username(), userAuthRequest.password());
+        return ResponseEntity.ok(authResponse);
+    }
+
+    @PostMapping(value = "/logout")
+    public ResponseEntity<Void> logout(@RequestBody Map<String, String> body) throws BadRequestException {
+        keycloakService.logout(body.get("refreshToken"));
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping(value = "/refresh")
+    public ResponseEntity<AuthResponse> refresh(@RequestBody Map<String, String> body) throws BadRequestException {
+        String refreshToken = body.get("refreshToken");
+        return ResponseEntity.ok(keycloakService.refreshToken(refreshToken));
+    }
+}
